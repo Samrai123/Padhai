44747d4d3701da80c6cc909feef4a3ef09ac35d9
Sambhrant Rai <84489316+Samrai123@users.noreply.github.com>
2024-03-25 16:41:39 +0545

    welcome_page_complete

---
+++ lib/auth/welcome_page.dart
@@ -0,0 +1,85 @@
+import 'package:eduhub/auth/login_main.dart';
+import 'package:eduhub/components/constants.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_sliding_up_panel/sliding_up_panel_widget.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:page_transition/page_transition.dart';
+
+class Welcome extends StatefulWidget {
+  const Welcome({super.key});
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: HexColor("#CCB1E6"),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Flexible(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      width: MediaQuery.sizeOf(context).width,
+                      child: Image.asset(
+                        'assets/images/onlineedu.png',
+                        width: 200,
+                        height: 200,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    const Text(
+                      "Welcome to Padhai",
+                      style: kHeadline,
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: const Text(
+                        "An app where you can find and become professional with your passion!",
+                        style: kBodyText,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      PageTransition(
+                          type: PageTransitionType.bottomToTop,
+                          child: Login()));
+                },
+                child: Image.asset(
+                  'assets/images/north.png',
+                  width: 50,
+                  height: 50,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/components/constants.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const kBackgroundColor = Color.fromARGB(255, 242, 242, 243);
+const kTextFieldFill = Color(0xff1E1C24);
+// TextStyles
+const kHeadline = TextStyle(
+    color: Color.fromARGB(255, 13, 11, 11),
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'CourierPrime');
+
+const kBodyText = TextStyle(
+    color: Color.fromARGB(255, 13, 11, 11),
+    fontSize: 15,
+    fontFamily: 'CourierPrime');
+
+const kButtonText = TextStyle(
+    color: Colors.black87,
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'CourierPrime');
+
+const kBodyText2 = TextStyle(
+    fontSize: 28,
+    fontFamily: 'CourierPrime',
+    fontWeight: FontWeight.w500,
+    color: Color.fromARGB(255, 13, 11, 11));
